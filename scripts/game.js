68325c7c857a3f77518805160ccc5d0b67011afe
Barry Mays <barrykmays@gmail.com>
2024-09-01 15:54:09 +0000

    Add game files and initial tests

---
+++ scripts/game.js
@@ -0,0 +1,21 @@
+let game = {
+    currentGame: [],
+    score: 0,
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"]
+};
+
+
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    document.getElementById("score").innerText = "0"
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore };
